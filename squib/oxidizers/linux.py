2c062924dba85822de378cd343cee895f5057716
Mark Crewson <mark.crewson@alea.ca>
2011-11-03 15:27:10 -0700

    Add a filesystem oxidizer to report of filesystem sizes. Added a 'units' configuration option to the traffic oxidizer to report traffic sizes in other units beside just 'bytes' (i.e., bits, mbytes, gbytes, etc).

--- squib/oxidizers/linux.py
+++ squib/oxidizers/linux.py
@@ -1,8 +1,9 @@
 # vim:set ts=4 sw=4 et nowrap syntax=python ff=unix:
 #
 
-import sys, time
+import os, sys, time
 
+from squib.core.config            import ConfigError
 from squib.core.string_conversion import convert_to_bool, ConversionError
 from squib.oxidizers.base         import PeriodicOxidizer
 
@@ -51,6 +52,44 @@ class FileDescriptorOxidizer (PeriodicOxidizer):
 
 ##############################################################################
 
+class FileSystemOxidizer (PeriodicOxidizer):
+
+    valid_fstypes = ('btrfs','ext2','ext3','ext4','ext4dev',
+                     'fat','jfs','minix','msdos', 'reiserfs',
+                     'reiserfs','ufs','vfat','xfs')
+
+    def run_once (self):
+        for filesystem in self.find_local_filesystems():
+            if filesystem == '/':
+                fsname = '<root>'
+            elif filesystem.startswith('/'):
+                fsname = filesystem[1:].replace('/', '_')
+            else:
+                fsname = filesystem.replace('/', '_')
+            fs = os.statvfs(filesystem)
+            print 'filesystem.%s.size.total %d' % (fsname, fs.f_frsize * fs.f_blocks)
+            print 'filesystem.%s.size.used %d' % (fsname, (fs.f_frsize * fs.f_blocks) - (fs.f_frsize * fs.f_bfree))
+            print 'filesystem.%s.size.free %d'  % (fsname, fs.f_frsize * fs.f_bfree)
+            print 'filesystem.%s.size.avail %d' % (fsname, fs.f_frsize * fs.f_bavail)
+            print 'filesystem.%s.inodes.total %d' % (fsname, fs.f_files)
+            print 'filesystem.%s.inodes.used %d' % (fsname, fs.f_files - fs.f_ffree)
+            print 'filesystem.%s.inodes.free %d'  % (fsname, fs.f_ffree)
+            print 'filesystem.%s.inodes.avail %d' % (fsname, fs.f_favail)
+        sys.stdout.flush()
+
+    def find_local_filesystems (self):
+        f = open('/proc/mounts', 'r')
+        lines = f.readlines()
+        f.close()
+        fs = []
+        for line in lines:
+            device,mountpoint,fstype = line.split()[:3]
+            if fstype in self.valid_fstypes:
+                fs.append(mountpoint)
+        return fs
+
+##############################################################################
+
 class InodeOxidizer (PeriodicOxidizer):
 
     def run_once (self):
@@ -89,6 +128,7 @@ class TrafficOxidizer (PeriodicOxidizer):
     def setup (self):
         super(TrafficOxidizer, self).setup()
         self.setup_loopback()
+        self.setup_units()
         self.prev_traf_stats = {}
 
     def setup_loopback (self):
@@ -100,6 +140,32 @@ class TrafficOxidizer (PeriodicOxidizer):
                 self.include_loopback = convert_to_bool(loopback)
             except ConversionError:
                 raise ConfigError('%s::include_loopback must be a boolean' % self.name)
+
+    def setup_units (self):
+        units = self.config.get('units')
+        if units is None:
+            self.units = 1
+            return
+
+        units = units.strip().lower()
+        if units == "bits":
+            self.units = 8.0
+        elif units == "kbits":
+            self.units = 8.0 / 1024
+        elif units == "mbits":
+            self.units = 8.0 / (1024 * 1024)
+        elif units == "gbits":
+            self.units = 8.0 / (1024 * 1024 * 1024)
+        elif units == "bytes":
+            self.units = 1.0
+        elif units == "kbytes":
+            self.units = 1.0 / 1024
+        elif units == "mbytes":
+            self.units = 1.0 / (1024 * 1024)
+        elif units == "gbytes":
+            self.units = 1.0 / (1024 * 1024 * 1024)
+        else:
+            raise ConfigError('%s::units must be one of: bits,kbits,mbits,gbits,bytes,kbytes,mbytes,gbytes' % self.name)
 
     def run_once (self):
         f = open('/proc/net/dev', 'r')
@@ -137,22 +203,22 @@ class TrafficOxidizer (PeriodicOxidizer):
                                                tbytes, tpackets, terrors, tdrops)
                 continue
             
-            rb_diff = rbytes - prev[0]
+            rs_diff = rbytes - prev[0]
             rp_diff = rpackets - prev[1]
             re_diff = rerrors - prev[2]
             rd_diff = rdrops - prev[3]
-            tb_diff = tbytes - prev[4]
+            ts_diff = tbytes - prev[4]
             tp_diff = tpackets - prev[5]
             te_diff = terrors - prev[6]
             td_diff = tdrops - prev[7]
             self.prev_traf_stats[iface] = (rbytes, rpackets, rerrors, rdrops, 
                                            tbytes, tpackets, terrors, tdrops)
 
-            print 'traffic.%s.rbytes meter +%d'   % (iface, rb_diff)
+            print 'traffic.%s.rsize meter +%d'    % (iface, rs_diff * self.units)
             print 'traffic.%s.rpackets meter +%d' % (iface, rp_diff)
             print 'traffic.%s.rerrors meter +%d'  % (iface, re_diff)
             print 'traffic.%s.rdrops meter +%d'   % (iface, rd_diff)
-            print 'traffic.%s.tbytes meter +%d'   % (iface, tb_diff)
+            print 'traffic.%s.tsize meter +%d'    % (iface, ts_diff * self.units)
             print 'traffic.%s.tpackets meter +%d' % (iface, tp_diff)
             print 'traffic.%s.terrors meter +%d'  % (iface, te_diff)
             print 'traffic.%s.tdrops meter +%d'   % (iface, td_diff)
