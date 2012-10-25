56f90319fb9bcfbd76e314a5a414289f5612315c
Mark Crewson <mark.crewson@alea.ca>
2012-10-25 11:37:44 -0700

    asyncnet refactor to add udp and multicast reactables

--- squib/core/asyncnet.py
+++ squib/core/asyncnet.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import errno, socket, sys
+import errno, socket, struct, sys
 
 try:
     from OpenSSL import SSL
@@ -22,18 +22,17 @@ try:
 except ImportError:
     ssl_supported = False
 
-from squib.core.async import Reactable
+from squib.core.async import Reactable, ProtocolMixin
 
 ##############################################################################
 
 class SocketReactable (Reactable):
 
-    connected = False
-    accepting = False
+    socket_family = None
+    socket_type   = None
 
-    def __init__ (self, addr=None, sock=None, reactor=None):
+    def __init__ (self, sock=None, reactor=None):
         super(SocketReactable, self).__init__(reactor=reactor)
-        self.address = addr
         if sock is not None:
             self.set_socket(sock)
 
@@ -42,48 +41,67 @@ class SocketReactable (Reactable):
         self.socket.setblocking(0)
         self.add_to_reactor()
 
-    def create_socket (self, family=socket.AF_INET, type=socket.SOCK_STREAM):
-        self.set_socket(socket.socket(family, type))
-
-    def on_accept (self, sock, addr):
-        pass
-
-    def on_connect (self):
-        pass
-
-    ##############################################
+    def create_socket (self, sfamily=None, stype=None):
+        if sfamily is None: sfamily = self.socket_family
+        if stype is None:   stype   = self.socket_type
+        self.set_socket(socket.socket(sfamily, stype))
 
     def fileno (self):
         return self.socket.fileno()
 
+    def handle_close (self):
+        try:
+            self.socket.close()
+        except socket.error, err:
+            if err.args[0] not in (errno.ENOTCONN, errno.EBADF):
+                raise
+
+    ##############################################
+
+    # Socket object methods
+
+    def bind (self, addr):
+        self.socket.bind(addr)
+
+    def set_reuse_addr (self):
+        try:
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,
+                                   self.socket.getsockopt(socket.SOL_SOCKET, 
+                                                          socket.SO_REUSEADDR) | 1)
+        except socket.error:
+            pass
+
+##############################################################################
+
+class TCPReactable (SocketReactable):
+
+    socket_family = socket.AF_INET
+    socket_type   = socket.SOCK_STREAM
+
+    connected = False
+
+    def on_connect (self):
+        pass
+
+    ########################################################################## 
+
+    def __init__ (self, address, sock=None, reactor=None):
+        super(TCPReactable, self).__init__(sock=sock, reactor=reactor)
+        self.address = address
+
     def writable (self):
         if not self.connected: return True
-        return super(SocketReactable, self).writable()
+        return super(TCPReactable, self).writable()
 
     def handle_read_event (self):
-        if self.accepting:
-            if not self.connected:
-                self.connected = True
-            self.handle_accept()
-            return
         if not self.connected:
             self.handle_connect()
-            self.connected = True
-        super(SocketReactable, self).handle_read_event()
+        super(TCPReactable, self).handle_read_event()
 
     def handle_write_event (self):
         if not self.connected:
             self.handle_connect()
-            self.connected = True
-        super(SocketReactable, self).handle_write_event()
-
-    def handle_accept (self):
-        try:
-            sock, addr = self.socket.accept()
-            self.on_accept(sock, addr)
-        except socket.error, err:
-            if err.args[0] != errno.EWOULDBLOCK:
-                raise
+        super(TCPReactable, self).handle_write_event()
 
     def handle_connect (self):
         assert self.address is not None, "no address specified for connect"
@@ -95,7 +113,11 @@ class SocketReactable (Reactable):
             self.connected = True
             self.on_connect()
         else:
-            raise socket.error(err, errorcode[err])
+            raise socket.error(err, errno.errorcode.get(err))
+
+    def handle_close (self):
+        self.connected = False
+        super(TCPReactable, self).handle_close()
 
     def handle_read (self):
         try:
@@ -114,43 +136,59 @@ class SocketReactable (Reactable):
                 raise
             return 0
 
-    def handle_close (self):
-        self.connected = False
-        self.accepting = False
+##############################################################################
+
+class TCPListener (TCPReactable):
+
+    accepting = False
+
+    def __init__ (self, address, sock=None, reactor=None):
+        super(TCPListener, self).__init__(address, sock, reactor)
+        if not sock:
+            self.create_socket()
+            self.set_reuse_addr()
+
+    def activate (self):
+        self.bind(self.address)
+        self.listen(5)
+
+    def on_accept (self, sock, addr):
+        pass
+
+    ##########################################################################
+
+    def writable (self):
+        return False
+
+    def handle_read_event (self):
+        if self.accepting:
+            self.handle_accept()
+        else:
+            super(TCPListener, self).handle_read_event()
+
+    def handle_accept (self):
         try:
-            self.socket.close()
+            sock, addr = self.socket.accept()
+            self.connected = True
+            self.on_accept(sock, addr)
         except socket.error, err:
-            if err.args[0] not in (errno.ENOTCONN, errno.EBADF):
+            if err.args[0] != errno.EWOULDBLOCK:
                 raise
-
-    ##############################################
-
-    # Socket object methods
-
-    def bind (self, addr):
-        self.socket.bind(addr)
 
     def listen (self, num):
         self.accepting = True
         return self.socket.listen(num)
 
-    def set_reuse_addr (self):
-        try:
-            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,
-                                   self.socket.getsockopt(socket.SOL_SOCKET, 
-                                                          socket.SO_REUSEADDR) | 1)
-        except socket.error:
-            pass
-
 ##############################################################################
 
 if ssl_supported == True:
-    class SSLSocketReactable (SocketReactable):
+
+    class SSLReactable (TCPReactable):
 
         def __init__ (self, cert, pkey, cacert=None, addr=None, sock=None, reactor=None):
             self.ssl_ctx = None
             self.set_ssl_certificate(cert, pkey, cacert)
-            super(SSLSocketReactable, self).__init__(addr, sock, reactor)
+            super(SSLReactable, self).__init__(addr, sock, reactor)
 
         def set_ssl_certificae (self, cert, pkey, cacert=None):
             self.set_ssl_context(self._create_ssl_context(cert, pkey, cacert))
@@ -159,7 +197,7 @@ if ssl_supported == True:
             self.ssl_ctx = ctx
 
         def create_socket (self, family, type):
-            sock = socket.socket(family, type)
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             if self.ssl_ctx is not None:
                 sock = SSL.Connection(self.ssl_ctx, sock)
             self.set_socket(sock)
@@ -167,7 +205,7 @@ if ssl_supported == True:
         def handle_read (self):
             while True:
                 try:
-                    return super(SSLSocketReactable, self).handle_read()
+                    return super(SSLReactable, self).handle_read()
                 except SSL.ZeroReturnError:
                     return ''
                 except SSL.WantReadError:
@@ -181,7 +219,7 @@ if ssl_supported == True:
         def handle_write (self, data):
             while True:
                 try:
-                    return super(SSLSocketReactable, self).handle_write(data)
+                    return super(SSLReactable, self).handle_write(data)
                 except SSL.SysCallError, e:
                     if e.args[0] == errno.EPIPE:
                         self.close()
@@ -221,37 +259,115 @@ if ssl_supported == True:
                 return True
             return False
 
+    class SSLServer (SSLReactable, TCPListener):
+
+        def __init__ (self, address, cert, pkey,
+                      cacert=None, sock=None, reactor=None):
+            SSLReactable.__init__(self, cert, pkey, cacert, sock, reactor)
+            TCPListener.__init__(self, address, sock, reactor)
+
 ##############################################################################
 
-class Server (SocketReactable):
+class UDPReactable (SocketReactable):
 
-    def __init__ (self, bind_address, protocol, sock=None, reactor=None):
-        self.bind_address = bind_address
-        self.protocol = protocol
-        super(Server, self).__init__(sock=sock, reactor=reactor)
+    socket_family = socket.AF_INET
+    socket_type   = socket.SOCK_DGRAM
+
+    def __init__ (self, address, sock=None, reactor=None):
+        super(UDPReactable, self).__init__(sock, reactor)
+        self.address = address
+
+    def handle_read (self):
+        try:
+            data,addr = self.socket.recvfrom(self.in_buffer_size)
+            return data
+        except socket.error, err:
+            if err.args[0] in (errno.EAGAIN, errno.EINTR, errno.EWOULDBLOCK):
+                return ''
+            else:
+                raise
+
+    def handle_write (self, data):
+        try:
+            sent = self.socket.sendto(data, self.address)
+            return sent
+        except socket.error, err:
+            if err.args[0] != errno.EWOULDBLOCK:
+                raise
+            return 0
+
+##############################################################################
+
+class UDPListener (UDPReactable):
+
+    def __init__ (self, address, sock=None, reactor=None):
+        super(UDPListener, self).__init__(address, sock, reactor)
         if not sock:
-            self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.create_socket()
             self.set_reuse_addr()
 
     def activate (self):
-        self.bind(self.bind_address)
-        self.listen(5)
-
-    def writable (self):
-        return False
-
-    def on_accept (self, sock, addr):
-        self.protocol(addr, sock, self.reactor)
+        self.bind(self.address)
 
 ##############################################################################
 
-if ssl_supported == True:
-    class SSLServer (SSLSocketReactable, Server):
+def MulticastReactable (UDPReactable):
+    joined_group = False
 
-        def __init__ (self, bind_address, protocol, cert, pkey,
-                      cacert=None, sock=None, reactor=None):
-            SSLSocketReactable.__init__(self, cert, pkey, cacert, sock, reactor)
-            Server.__init__(self, bind_address, protocol, sock, reactor)
+    def on_join_group (self):
+        pass
+
+    def on_leave_group (self):
+        pass
+
+    def set_loopback_mode (self, mode)
+        mode = struct.pack('b', mode)
+        self.socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, mode)
+
+    def set_ttl (self, ttl)
+        ttl = struct.pack('B', ttl)
+        self.socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+    ##########################################################################
+
+    def __init__ (self, address, interface='', sock=None, reactor=None):
+        super(MulticastReactable, self).__init__(address, sock, reactor)
+        self.interface = interface
+
+    def handle_read_event (self):
+        if not self.joined_group:
+            self.handle_join_group()
+        super(MulticastReactable, self).handle_read_event()
+
+    def handle_write_event (self):
+        if not self.joined_group():
+            self.handle_join_group()
+        super(MulticastReactable, self).handle_write_event()
+
+    def handle_close (self):
+        if self.joined_group:
+            self.handle_leave_group()
+        super(MulticastReactable, self).handle_close()
+
+    def handle_join_group (self):
+        self._multicast_join_group(self.address[0], self.interface, True)
+        self.joined_group = True
+        self.on_join_group()
+
+    def handle_leave_group (self):
+        self._multicast_join_group(self.address[0], self.interface, False)
+        self.joined_group = False
+        self.on_leave_group()
+
+    def _multicast_join_group (self, address, interface, join):
+        addr = socket.inet_aton(socket.gethostbyname(address))
+        intf = socket.inet_aton(socket.gethostbyname(interface))
+        if join:
+            cmd = socket.IP_ADD_MEMBERSHIP
+        else:
+            cmd = socket.IP_DROP_MEMBERSHIP
+        try:
+            self.socket.setsockopt(socket.IPPROTO_IP, cmd, addr + intf)
 
 ##############################################################################
 
@@ -259,14 +375,13 @@ def __test ():
 
     from squib.core.async import get_reactor, LineOrientedProtocolMixin, TimeoutMixin
 
-    class ChatProtocol (SocketReactable, LineOrientedProtocolMixin, TimeoutMixin):
+    class ChatProtocol (TCPReactable, LineOrientedProtocolMixin, TimeoutMixin):
 
         channels = dict()
-
         idletime = 10
 
-        def __init__ (self, sock, addr, reactor):
-            super(ChatProtocol, self).__init__(sock, addr, reactor)
+        def __init__ (self, address, sock, reactor=None):
+            super(ChatProtocol, self).__init__(address, sock, reactor)
             ChatProtocol.channels[self] = 1
             self.nick = None
             self.write_data('nickname: ')
@@ -336,7 +451,11 @@ def __test ():
                 nicks = [ x.nick or '<unknown>' for x in ChatProtocol.channels.keys() ]
                 self.write_data(' ' + '\r\n '.join(nicks) + '\r\n')
 
-    Server(bind_address=('', 8518), protocol=ChatProtocol).activate()
+    class ChatServer (TCPListener):
+        def on_accept (self, sock, addr):
+            ChatProtocol(addr, sock)
+
+    ChatServer(address=('', 8518)).activate()
     get_reactor().start()
 
 if __name__ == "__main__":
